39802bee63873c4d81449cd10fddd6fee5753bac
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-03-23 11:26:55 +0100

    Eslint ignore.

--- src/static/wallet.js
+++ src/static/wallet.js
@@ -1,9 +1,9 @@
-// import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
-
 async function connectWallet() {
   // Your code here...
   // A Web3Provider wraps a standard Web3 provider, which is
   // what MetaMask injects as window.ethereum into each page
+
+  // eslint-disable-next-line no-use-before-define
   const provider = new ethers.providers.Web3Provider(window.ethereum);
 
   // MetaMask requires requesting permission to connect users accounts
@@ -13,5 +13,4 @@ async function connectWallet() {
   // send ether and pay to change state within the blockchain.
   // For this, you need the account signer...
   const signer = provider.getSigner();
-  console.log(signer);
 }
